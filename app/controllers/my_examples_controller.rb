7a29692163f9cb8b62e89a9c0a60b1198e09bd4a
schristinelee <christinelee1527@gmail.com>
2022-11-08 02:11:09 -0800

    all the exercises

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,33 +1,33 @@
 class MyExamplesController < ApplicationController
   def fortune_method
-    render json: fortunes = ["You will gen an error!", "You won't get any errors!", "You will have several errors!"].sample
+    fortunes = ["You will gen an error!", "You won't get any errors!", "You will have several errors!"]
+    render json: { fortune: fortunes.sample }
   end
 
-  def lotto_numbers
-    lotto_numbers = []
+  def lotto_method
+    numbers = []
     6.times do
-      number = rand(1..60)
-      lotto_numbers << number
+      numbers << rand(1..60)
     end
-    render json: { lotto_numbers: lotto_numbers }
+    render json: { lotto_numbers: numbers }
   end
 
-  def counter_action
+  def counter_method
     count = 0
-    count += 1
+    count += 1 #this doesn't actually work
     render json: { number_of_visits: count }
   end
 
-  def beer_lyrics
+  def beer_method
     lyrics = []
     number = 99
-    100.times do
-      if number == 0
-        lyrics << "No more bottles of beer on the wall, no more bottles of beer.
-        Go to the store and buy some more, 99 bottles of beer on the wall."
-        # elsif number
-      end
+    97.times do
+      message = "#{number} bottles of beer on the wall, #{number} bottles of beer. Take one down and pass it around, #{number - 1} bottles of beer on the wall."
+      lyrics << message
+      number -= 1
     end
-    render json: { beer_lyrics: beer_lyrics }
+    message = "2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around, 1 bottle of beer on the wall. 1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall. No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
+    lyrics << message
+    render json: { message: lyrics }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get "/fortune_path", controller: "my_examples", action: "fortune_method"
+  get "/lotto_path", controller: "my_examples", action: "lotto_method"
+  get "/counter_path", controller: "my_examples", action: "counter_method"
+  get "/beer_path", controller: "my_examples", action: "beer_method"
 end
